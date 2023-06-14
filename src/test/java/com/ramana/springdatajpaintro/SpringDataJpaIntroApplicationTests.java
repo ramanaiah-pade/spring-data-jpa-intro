922ec28d0fb2bf5d0bab36a3139d7570c8215e5f
ramanaiah-pade <ramanaiah.pade1921@gmail.com>
2023-06-14 19:33:57 +0530

    Initial project

---
+++ src/test/java/com/ramana/springdatajpaintro/SpringDataJpaIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramana.springdatajpaintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
